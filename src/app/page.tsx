1c8c70a342eff3833f1202d29654d3dc8f526574
Emmanuel Lion <emmanuel.lion30@gmail.com>
2025-01-23 10:59:14 +0100

    initial commit 🚀

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "antd";
+
+export default function Home() {
+  return (
+    <div className={"w-screen h-screen flex items-center justify-center"}>
+      <Button color="default" variant="solid">
+        Solid
+      </Button>
+    </div>
+  );
+}
